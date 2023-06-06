ff20ab9ef1a3c9b4a1d6671bd539cf49508db5b7
Denis karpovskii <geext29@gmail.com>
2023-06-06 10:11:24 +0200

    Implement basic products service tests

--- cdk-backend/lib/products-service.get-products-by-id.ts
+++ cdk-backend/lib/products-service.get-products-by-id.ts
@@ -1,9 +1,9 @@
-import { APIGatewayEvent, Handler } from "aws-lambda";
+import { APIGatewayEvent, APIGatewayProxyHandler, Handler } from "aws-lambda";
 import getProductsById from "../../src/products/get-products-by-id";
 
 export const productPathParameter = "productId";
 
-export const handler: Handler = async (event: APIGatewayEvent) => {
+export const handler: APIGatewayProxyHandler = async (event) => {
   const productId = event.pathParameters?.[productPathParameter];
   const product = productId ? await getProductsById(productId) : undefined;
 
@@ -19,6 +19,7 @@ export const handler: Handler = async (event: APIGatewayEvent) => {
 
   return {
     statusCode: 404,
+    body: "",
     headers: {
       "Access-Control-Allow-Origin": "*",
     },
